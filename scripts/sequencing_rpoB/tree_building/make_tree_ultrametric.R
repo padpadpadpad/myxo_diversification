c7517880c731b12178786b0898c5e99aa551f21d
padpadpadpad <d.padfieldscfc@gmail.com>
2022-10-20 22:05:54 +0100

    use treepl tree for bamm not ultrametric version as it messes with the tree

--- scripts/sequencing_rpoB/tree_building/make_tree_ultrametric.R
+++ scripts/sequencing_rpoB/tree_building/make_tree_ultrametric.R
@@ -7,8 +7,17 @@ tree <- read.tree('data/sequencing_rpoB/treepl/myxo_treepl_smooth10.tree')
 
 # check if ultrametric
 is.ultrametric(tree)
+is.rooted(tree)
 
 # make tree ultrametric
 tree_ultrametric <- force.ultrametric(tree)
 
+is.rooted(tree_ultrametric)
+
 write.tree(tree_ultrametric, 'data/sequencing_rpoB/bamm/myxo_treepl_smooth10_ultrametric.tree')
+
+tree <- read.tree('data/sequencing_rpoB/bamm/myxo_treepl_smooth10_ultrametric.tree')
+
+is.rooted(tree)
+
+# this does not look great LOL.
