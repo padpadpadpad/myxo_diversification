48846e9ca05a1bef78ef1726fc6c762d7e73c717
padpadpadpad <d.padfieldscfc@gmail.com>
2022-10-24 20:22:57 +0100

    re-run bamm but stopped as other run converged

--- scripts/sequencing_rpoB/analyses/run_bamm.sh
+++ scripts/sequencing_rpoB/analyses/run_bamm.sh
@@ -6,3 +6,11 @@ cd data/sequencing_rpoB/bamm
 
 # run bamm
 bamm -c bamm_control.txt
+
+# run bamm for sampling fraction of .4
+screen -S bamm_percent40
+
+cd data/sequencing_rpoB/bamm
+
+# run bamm
+bamm -c bamm_control_0.4frac.txt
