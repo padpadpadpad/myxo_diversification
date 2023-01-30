472d10f3c4e7bca05915b44691c44dee1c583d7e
Daniel Padfield <d.padfieldscfc@gmail.com>
2023-01-30 20:23:44 +0000

    Update alignments and constraint trees

--- scripts/sequencing_rpoB/tree_building/raxml_new.sh
+++ scripts/sequencing_rpoB/tree_building/raxml_new.sh
@@ -1,27 +1,16 @@
 # if not done already - create environment for running RAXML
 # install raxml-ng into it
 
-# check raxml env
-conda env list
-
-# create an environment for raxml-ng
-mamba create -n raxml_env 
-
-# open the environment
-conda activate raxml_env
-
-mamba install -c bioconda raxml-ng=1.1.0
-
 # create screen
 screen -S raxml
 
 conda activate raxml_env
 
 # create new folder for output
-cd ~/dada2_pipeline_myxorpoB/data/output/run_myxo_gtdbr202
-mkdir -p raxml/myxo_97.7_constraint
+cd ~/raxml_new
+mkdir -p trees/myxo_asv
 
 # check raxml - run it with a check first and with constraints
-raxml-ng --check --msa alignments/new/alignment_myxo_97.7percent_new.fasta --model GTR+G --prefix raxml/myxo_97.7_constraint --threads 20 --seed 2 --tree-constraint alignments/new/contree.tre --outgroup otu_outgroup1
+raxml-ng --check --msa alignment/alignment_asv.fasta --model GTR+G --prefix trees/myxo_asv/myxo_asv --threads 20 --seed 2 --tree-constraint constraint_trees/constraint_tree_asv.tre
 
-raxml-ng --msa alignments/new/alignment_myxo_97.7percent_new.fasta --model GTR+G --prefix raxml/myxo_97.7_constraint --threads 20 --seed 2 --tree-constraint alignments/new/contree.tre --outgroup otu_outgroup1
+raxml-ng --msa alignment/alignment_asv.fasta --model GTR+G --prefix trees/myxo_asv/myxo_asv --threads 20 --seed 2 --tree-constraint constraint_trees/constraint_tree_asv.tre
