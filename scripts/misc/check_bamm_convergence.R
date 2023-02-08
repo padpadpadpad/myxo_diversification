0db43889f7499d7878c3c7d48610feb91449b87d
padpadpadpad <d.padfieldscfc@gmail.com>
2023-02-08 16:09:40 +0000

    run bamm again

---
+++ scripts/misc/check_bamm_convergence.R
@@ -0,0 +1,20 @@
+# check bamm run convergence
+
+# load packages
+library(BAMMtools)
+library(coda)
+
+# set file
+file <- 'data/sequencing_rpoB/bamm/bamm_asv_mcmc_out.txt'
+
+# read in mcmc output
+mcmcout <- read.csv(file, header=TRUE)
+max(mcmcout$generation)
+
+# discard some runs as burnin. We will discard the first 10% of samples
+burnstart <- floor(0.5 * nrow(mcmcout))
+postburn <- mcmcout[burnstart:nrow(mcmcout), ]
+
+# calculate effective sample size
+effectiveSize(postburn$N_shifts)
+effectiveSize(postburn$logLik)
