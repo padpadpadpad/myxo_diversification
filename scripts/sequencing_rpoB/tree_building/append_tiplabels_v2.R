10aa8b3dfa4e361f43912d0f1dfc9bd9c5e3a2c7
padpadpadpad <d.padfieldscfc@gmail.com>
2024-01-22 17:15:07 +0000

    Look at bootstrapped trees for myxo ASV level.

---
+++ scripts/sequencing_rpoB/tree_building/append_tiplabels_v2.R
@@ -0,0 +1,58 @@
+# add family to otu names to allow grouping in FigTree to root the tree properly
+
+#--------------------------#
+# what this script does ####
+#--------------------------#
+
+# load in packages
+library(phyloseq)
+library(tidyverse)
+library(here)
+
+here::i_am('scripts/sequencing_rpoB/tree_building/append_tiplabels_v2.R')
+
+# set percent similarity - those used in asvs_to_otus.R
+# at this point the trees failed at 90, 85, and 80%
+percent_similarity <- c('asv')
+
+# read in sequence to otu name converter
+asv_names <- read_csv(here('data/sequencing_rpoB/phyloseq/myxococcus/myxo_seq_name_conversion.csv'))
+asv_names
+
+# run for loop to align sequences
+for(i in 1:length(percent_similarity)){
+  
+  # define which level of OTU similarity we are using
+  otu_similarity <- paste(percent_similarity[i], 'percent', sep = '')
+  if(percent_similarity[i] == 'asv'){otu_similarity = 'asv'}
+  
+  # load in myxo phyoseq object
+  ps_myxo <- readRDS(here(paste('data/sequencing_rpoB/phyloseq/myxococcus/prevalence_filtered/ps_otu_', otu_similarity,  '_filt.rds', sep = '')))
+  
+  # get taxonomy table from ps object
+  tax_table <- tax_table(ps_myxo) %>%
+    data.frame() %>%
+    rownames_to_column(var = 'tip_label') %>%
+    janitor::clean_names()
+  
+  # make this a temp file
+  temp_file <- tempfile()
+  write.csv(tax_table, temp_file, row.names = FALSE)
+  
+  # define tree file
+  in_tree <- here(paste('data/sequencing_rpoB/raxml/trees/myxo_', percent_similarity[i], '/myxo_', percent_similarity[i],'.raxml_tbe.tree', sep = ''))
+  
+  # define output file
+  out_tree <- here(paste('data/sequencing_rpoB/raxml/trees/myxo_', percent_similarity[i], '/myxo_', percent_similarity[i],'.raxml_tbe_family.tre', sep = ''))
+  
+  # define script file
+  script_file <- here('scripts/sequencing_rpoB/tree_building/extend_labels.pl')
+  
+  # run perl script to create constraint tree
+  system(paste('perl' , script_file, '-o', temp_file, '-t', in_tree, '>', out_tree, sep = ' '))
+  
+}
+
+
+
+
--- scripts/sequencing_rpoB/tree_building/bootstrap_raxml.sh
+++ scripts/sequencing_rpoB/tree_building/bootstrap_raxml.sh
@@ -17,3 +17,11 @@ ls
 
 # running standard RaxML
 raxml-ng --msa alignment_asv.fasta --model GTR+G --prefix boots --threads 25 --bootstrap --bs-trees autoMRE{500} --seed 42 --tree-constraint constraint_tree_asv.tre
+
+cd myxo_97.7
+
+# running standard RaxML
+raxml-ng --msa alignment_97.7percent.fasta --model GTR+G --prefix boots --threads 25 --bootstrap --bs-trees autoMRE{500} --seed 42 --tree-constraint constraint_tree_97.7percent.tre
+
+# running standard RaxML
+raxml-ng --msa alignment_95percent.fasta --model GTR+G --prefix boots --threads 25 --bootstrap --bs-trees autoMRE{500} --seed 42 --tree-constraint constraint_tree_95percent.tre
