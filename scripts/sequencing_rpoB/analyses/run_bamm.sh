ead0ee9c7ce9cfc3f9034415d99c0fb51c878d68
padpadpadpad <d.padfieldscfc@gmail.com>
2024-01-26 10:02:31 +0000

    add bamm script for running on multiple screens

--- scripts/sequencing_rpoB/analyses/run_bamm.sh
+++ scripts/sequencing_rpoB/analyses/run_bamm.sh
@@ -1,1 +1,41 @@
 # run bamm on multiple screens
+
+# open screen
+screen -S bamm_sf1
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/asv/bamm
+
+bamm -c bamm_control_sf1.txt
+
+# run sf0.5
+screen -S bamm_sf0.5
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/asv/bamm
+
+bamm -c bamm_control_sf0.5.txt
+
+# run sf0.25
+screen -S bamm_sf0.25
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/asv/bamm
+
+bamm -c bamm_control_sf0.25.txt
+
+# run sf0.125
+screen -S bamm_sf0.125
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/asv/bamm
+
+bamm -c bamm_control_sf0.125.txt
+
+# run sf0.0625
+screen -S bamm_sf0.0625
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/asv/bamm
+
+bamm -c bamm_control_sf0.0625.txt
