21cb22f7d413235863191bb13d30e1b8bbb0deba
padpadpadpad <d.padfieldscfc@gmail.com>
2024-01-26 10:02:03 +0000

    add 95% otu bootstrapped trees

--- scripts/sequencing_rpoB/tree_building/pick_bootstrap_trees.R
+++ scripts/sequencing_rpoB/tree_building/pick_bootstrap_trees.R
@@ -6,6 +6,8 @@ librarian::shelf(ape, tidyverse)
 # set seed
 set.seed(42)
 
+# first for ASVs ####
+
 # load in trees
 trees <- read.tree('data/sequencing_rpoB/raxml/trees/myxo_asv/boots.raxml.bootstraps')
 
@@ -39,3 +41,43 @@ trees2[[1]]$tip.label[1:6]
 trees[[9]]$tip.label[1:6]
 trees2[[9]]$tip.label[1:6]
 
+#------------------------#
+# now for 95% cut-off ####
+#------------------------#
+
+# load in trees
+trees <- read.tree('data/sequencing_rpoB/raxml/trees/myxo_95/boots.raxml.bootstraps')
+
+# pick 9 random trees
+trees <- trees[sample(1:length(trees), 9)]
+
+# load in rooted tree with family name tip labels
+tree1 <- read.nexus('data/sequencing_rpoB/raxml/trees/myxo_95/myxo_95.raxml_rooted.tre')
+
+# write function to remove family labels
+strsplit_mod <- function(x)(strsplit(x, split = '_') %>% unlist() %>% .[1:2] %>% paste0(., collapse = '_'))
+
+# extract original tip labels
+tree1_tiplabels <- tibble(tree1_label = tree1$tip.label,
+                          tree2_label = purrr::map_chr(tree1_label, strsplit_mod
+                          ))
+
+trees2 <- trees
+
+for(i in 1:9){
+  temp <- tibble(tree2_label = trees[[i]]$tip.label) %>%
+    left_join(., tree1_tiplabels)
+  trees[[i]]$tip.label <- temp$tree1_label
+  
+  # save out tree
+  write.tree(trees[[i]], paste0('data/sequencing_rpoB/raxml/trees/myxo_95//raxml_bootstrap_', i, '.tree'))
+}
+
+trees[[1]]$tip.label[1:6]
+trees2[[1]]$tip.label[1:6]
+trees[[9]]$tip.label[1:6]
+trees2[[9]]$tip.label[1:6]
+
+#--------------------------#
+# now for 97.7% cut-off ####
+#--------------------------#
