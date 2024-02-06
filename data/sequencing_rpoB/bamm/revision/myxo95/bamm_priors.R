68c918fad1e01d392553b5202b165f351455034c
padpadpadpad <d.padfieldscfc@gmail.com>
2024-02-06 16:39:58 +0000

    Run bamm on 95 and 97.7 cut off trees

--- data/sequencing_rpoB/bamm/revision/myxo95/bamm_priors.R
+++ data/sequencing_rpoB/bamm/revision/myxo95/bamm_priors.R
@@ -5,9 +5,13 @@ librarian::shelf(BAMMtools, ape)
 # load in tree
 tree <- read.tree('data/sequencing_rpoB/raxml/trees/myxo_95/myxo_95_treepl_cv_node_labels.tre')
 
+ape::ltt.plot(tree, log = 'y')
+
 setBAMMpriors(tree, outfile = 'data/sequencing_rpoB/bamm/revision/myxo95/bamm_priors.txt')
 
 # load in tree
 tree <- read.tree('data/sequencing_rpoB/raxml/trees/myxo_97.7/myxo_97.7_treepl_cv_node_labels.tre')
+
+ape::ltt.plot(tree, log = 'y')
 
 setBAMMpriors(tree, outfile = 'data/sequencing_rpoB/bamm/revision/myxo97.7/bamm_priors.txt')
--- scripts/sequencing_rpoB/analyses/run_bamm.sh
+++ scripts/sequencing_rpoB/analyses/run_bamm.sh
@@ -39,3 +39,83 @@ screen -S bamm_sf0.0625
 cd ~/raxml_mbe_response/asv/bamm
 
 bamm -c bamm_control_sf0.0625.txt
+
+# open screen
+screen -S bamm_sf1
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_95/bamm
+
+bamm -c bamm_control_sf1.txt
+
+# run sf0.5
+screen -S bamm_sf0.5
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_95/bamm
+
+bamm -c bamm_control_sf0.5.txt
+
+# run sf0.25
+screen -S bamm_sf0.25
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_95/bamm
+
+bamm -c bamm_control_sf0.25.txt
+
+# run sf0.125
+screen -S bamm_sf0.125
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_95/bamm
+
+bamm -c bamm_control_sf0.125.txt
+
+# run sf0.0625
+screen -S bamm_sf0.0625
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_95/bamm
+
+bamm -c bamm_control_sf0.0625.txt
+
+# open screen
+screen -S bamm_sf1
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_97.7/bamm
+
+bamm -c bamm_control_sf1.txt
+
+# run sf0.5
+screen -S bamm_sf0.5
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_97.7/bamm
+
+bamm -c bamm_control_sf0.5.txt
+
+# run sf0.25
+screen -S bamm_sf0.25
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_97.7/bamm
+
+bamm -c bamm_control_sf0.25.txt
+
+# run sf0.125
+screen -S bamm_sf0.125
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_97.7/bamm
+
+bamm -c bamm_control_sf0.125.txt
+
+# run sf0.0625
+screen -S bamm_sf0.0625
+
+# move to correct folder on cluster
+cd ~/raxml_mbe_response/myxo_97.7/bamm
+
+bamm -c bamm_control_sf0.0625.txt
