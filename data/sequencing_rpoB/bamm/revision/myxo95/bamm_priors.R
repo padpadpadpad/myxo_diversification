ece68cfb95c2ee79b89bd0ab9768b70675564e49
padpadpadpad <d.padfieldscfc@gmail.com>
2024-02-06 16:06:47 +0000

    Upload bamm configuaration files for 95 and 95.7 otu cutoff values for the best tree only

---
+++ data/sequencing_rpoB/bamm/revision/myxo95/bamm_priors.R
@@ -0,0 +1,13 @@
+# try and set bamm priors
+
+librarian::shelf(BAMMtools, ape)
+
+# load in tree
+tree <- read.tree('data/sequencing_rpoB/raxml/trees/myxo_95/myxo_95_treepl_cv_node_labels.tre')
+
+setBAMMpriors(tree, outfile = 'data/sequencing_rpoB/bamm/revision/myxo95/bamm_priors.txt')
+
+# load in tree
+tree <- read.tree('data/sequencing_rpoB/raxml/trees/myxo_97.7/myxo_97.7_treepl_cv_node_labels.tre')
+
+setBAMMpriors(tree, outfile = 'data/sequencing_rpoB/bamm/revision/myxo97.7/bamm_priors.txt')
